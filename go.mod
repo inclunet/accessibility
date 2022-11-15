6542ecb6cad6cd519e8bffe2a9d215346d0f2e91
Leonardo Gleison <leonardo.gleison@gmail.com>
2022-11-15 20:23:54 -0300

    Initial commit with check accessibility images on command line only

---
+++ go.mod
@@ -0,0 +1,9 @@
+module github.com/inclunet/accessibility
+
+go 1.17
+
+require (
+	github.com/PuerkitoBio/goquery v1.8.0 // direct
+	github.com/andybalholm/cascadia v1.3.1 // indirect
+	golang.org/x/net v0.0.0-20210916014120-12bc252f5db8 // indirect
+)
---
+++ main.go
@@ -0,0 +1,89 @@
+package main
+
+import (
+	"bytes"
+	"errors"
+	"flag"
+	"fmt"
+	"io"
+	"log"
+	"net/http"
+
+	"github.com/PuerkitoBio/goquery"
+	"github.com/inclunet/accessibility/pkg/images"
+	"github.com/inclunet/accessibility/pkg/summary"
+)
+
+var AccessibilityChecks []summary.AccessibilityCheck
+var Html string
+
+func AccessibilityCheckResult(s *goquery.Selection, a int, pass bool, description string) {
+	element := goquery.NodeName(s)
+	html, _ := goquery.OuterHtml(s)
+	AccessibilityChecks = append(AccessibilityChecks, summary.AccessibilityCheck{Element: element, A: a, Pass: pass, Description: description, Html: html})
+}
+
+func CheckImageAccessibility(i int, s *goquery.Selection) {
+	a, pass, description := images.Check(s)
+	AccessibilityCheckResult(s, a, pass, description)
+}
+
+func GetPage(url string) (*http.Response, error) {
+	Response, err := http.Get(url)
+
+	if err != nil {
+		return nil, err
+	}
+
+	defer Response.Body.Close()
+
+	if Response.StatusCode != 200 {
+		return nil, errors.New("URL not found")
+	}
+
+	Body, err := io.ReadAll(Response.Body)
+
+	if err != nil {
+		return nil, err
+	}
+
+	Html = string(Body)
+
+	Response.Body = io.NopCloser(bytes.NewBuffer(Body))
+
+	return Response, nil
+}
+
+func SaveResults() {
+	for _, entry := range summary.Generate(AccessibilityChecks) {
+		log.Printf("%d %s tested with %d errors and %d asserts", entry.Total, entry.Element, entry.Errors, entry.Pass)
+	}
+}
+
+func main() {
+	var url string
+	flag.StringVar(&url, "url", "https://inclunet.com.br", "Url to check accessibility")
+	flag.Parse()
+	fmt.Println(url)
+	log.Printf("Starting page evaluation process for %s", url)
+
+	Response, err := GetPage(url)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	document, err := goquery.NewDocumentFromReader(Response.Body)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	title := document.Find("title").Text()
+
+	log.Printf("Evaluating page with title: %s", title)
+
+	document.Find("img").Each(CheckImageAccessibility)
+
+	SaveResults()
+}
