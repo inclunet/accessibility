e695a46ccdade561adf09b8a7011eb1d4ec1f276
Leonardo Gleison <leonardo.gleison@gmail.com>
2022-11-27 12:08:25 -0300

    Add an element file.go for separate struct

--- pkg/accessibility/accessibility.go
+++ pkg/accessibility/accessibility.go
@@ -1,72 +1,9 @@
 package accessibility
 
-import (
-	"github.com/PuerkitoBio/goquery"
-)
-
 type Accessibility interface {
 	AlternativeText() (string, bool)
 	AriaHidden() bool
 	AriaLabel() (string, bool)
 	Check() (int, bool, string)
 	Role() (string, bool)
-}
-
-type Element struct {
-	Selection *goquery.Selection
-}
-
-func (e *Element) AlternativeText() (string, bool) {
-	if accessibleText, ok := e.Selection.Attr("alt"); ok && accessibleText != "" {
-		return accessibleText, true
-	}
-	return "", false
-}
-
-func (e *Element) AriaHidden() bool {
-	if value, ok := e.Selection.Attr("aria-hidden"); ok && value == "true" {
-		return true
-	}
-	return false
-}
-
-func (e *Element) AriaLabel() (string, bool) {
-	if value, ok := e.Selection.Attr("aria-label"); ok && value != "" {
-		return value, true
-	}
-	return "", false
-}
-
-func (e *Element) AccessibleText() (string, bool) {
-	if accessibleText, ok := e.AriaLabel(); ok {
-		return accessibleText, ok
-	}
-
-	if accessibleText, ok := e.AlternativeText(); ok {
-		return accessibleText, ok
-	}
-
-	if accessibleText := e.Selection.Text(); accessibleText != "" {
-		return accessibleText, true
-	}
-
-	if accessibleText, ok := e.Title(); ok {
-		return accessibleText, ok
-	}
-
-	return "", false
-}
-
-func (e *Element) Role() (string, bool) {
-	if role, ok := e.Selection.Attr("role"); ok && role != "" {
-		return role, ok
-	}
-	return "", false
-}
-
-func (e *Element) Title() (string, bool) {
-	if accessibleText, ok := e.Selection.Attr("title"); ok && accessibleText != "" {
-		return accessibleText, true
-	}
-	return "", false
 }
